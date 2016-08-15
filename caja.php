755f1f86566e1089fd8d97cf9c7c83b9cbc289bb
moonorongo <mscifu@gmail.com>
2016-08-15 09:33:38 -0300

    * agujero de seguridad: era posible realizar extracciones sin ser un usuario habilitado para ello. CORREGIDO + log para informar cuando un usuario ha intentado realizar una extraccion sin permiso

--- caja.php
+++ caja.php
@@ -3,51 +3,69 @@
     Caja Controller
 */    
     session_start();
+    require_once($_SERVER["DOCUMENT_ROOT"] ."/script/inc/config.php");
+    require_once($_SERVER["DOCUMENT_ROOT"] ."/script/inc/log4php/Logger.php");
+    Logger::configure($_SERVER["DOCUMENT_ROOT"] ."/script/inc/log4php.xml");
+    $log = Logger::getLogger('rootLogger');
+    
+    
     if(!isset($_SESSION['LOGGED'])) { // si no esta logueado
         header('HTTP/1.0 401 Unauthorized');
         echo ('NO AUTORIZADO: click <a href="http://'. $_SERVER['SERVER_NAME'] .'">aqui</a> para entrar');
     } else { // si esta logueado
-        require_once($_SERVER["DOCUMENT_ROOT"] ."/script/inc/config.php");
-        header('Content-Type: application/json');
-    
-        $mysql = Mysql::getInstance();
-        $mysql->connect();
-
-        $caja = new Caja($mysql);
-        $action = (isset($_REQUEST["action"]))? $_REQUEST["action"] : null;
-
+        if (isAdmin()) {
+            header('Content-Type: application/json');
         
-        if($action == 'cajaHandler') {
-            $model = json_decode($_REQUEST["model"]);
+            $mysql = Mysql::getInstance();
+            $mysql->connect();
 
-            if($model->sign == 1) {
-                $caja->registrarIngreso($model->monto, $model->observaciones);
-            } else {
-                $caja->registrarEgreso($model->monto, $model->observaciones);
+            $caja = new Caja($mysql);
+            $action = (isset($_REQUEST["action"]))? $_REQUEST["action"] : null;
+
+            
+            if($action == 'cajaHandler') {
+                $model = json_decode($_REQUEST["model"]);
+
+                if($model->sign == 1) {
+                    $caja->registrarIngreso($model->monto, $model->observaciones);
+                } else {
+                    $caja->registrarEgreso($model->monto, $model->observaciones);
+                }
+                echo '{"success" : '. $model->sign .'}';
             }
-            echo '{"success" : '. $model->sign .'}';
-        }
 
 
-        if($action == 'cajaListHandler') {
-            $fecha = $_REQUEST["fecha"];
-            $cList = $caja->listEgresosMes($fecha);
-            echo json_encode($cList);
-        }    
-        
+            if($action == 'cajaListHandler') {
+                $fecha = $_REQUEST["fecha"];
+                $cList = $caja->listEgresosMes($fecha);
+                echo json_encode($cList);
+            }    
+            
 
-        
-        if($action == 'cerrarCajaHandler') {
-            $success = true;
-            $year = $_REQUEST["year"];
-            $month = intval($_REQUEST["month"]);
-            $nextMonth = $month + 1;
-            $monto = $caja->obtenerSaldoMes($year, $month);
-            $success = $caja->registrarSaldoInicial($year .'-'. $nextMonth, $monto['monto']);
-            echo '{ "success" : '. $success .'}';
-        }    
+            
+            if($action == 'cerrarCajaHandler') {
+                $success = true;
+                $year = $_REQUEST["year"];
+                $month = intval($_REQUEST["month"]);
+                $monto = $caja->obtenerSaldoMes($year, $month);
+                
+                if ($month <> 12) {
+                    $nextMonth = $month + 1;
+                } else {
+                    $nextMonth = 1;
+                    $year++;
+                }
+                $success = $caja->registrarSaldoInicial($year .'-'. $nextMonth, $monto['monto']);
+                echo '{ "success" : '. $success .'}';
+            }    
 
-        
-        $mysql->close();    
+            
+            $mysql->close();                
+        } else  {
+            // no es admin, no tiene permiso
+            echo ('NO AUTORIZADO - debe ser administrador para ingresar');
+            $log->warn("NO AUTORIZADO - debe ser administrador para ingresar");
+        } // end is_admin
+
     } // if logged    
 ?>
